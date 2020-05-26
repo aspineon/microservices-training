39073d4bf7151ee416a859718e81f9e4858381bc
landrzejewski <landrzejewski.poczta@gmail.com>
2020-05-26 11:23:13 +0200

    Initial project

---
+++ payments/src/main/java/pl/training/microservices/payments/PaymentsRepository.java
@@ -0,0 +1,6 @@
+package pl.training.microservices.payments;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentsRepository extends JpaRepository<Payment, Long> {
+}
