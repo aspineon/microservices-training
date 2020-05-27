c5ba29e56c6732a09d74ef790dd78b01433c2af3
landrzejewski <landrzejewski.poczta@gmail.com>
2020-05-27 09:44:43 +0200

    Gateway

--- payments/src/main/java/pl/training/microservices/payments/PaymentsRepository.java
+++ payments/src/main/java/pl/training/microservices/payments/PaymentsRepository.java
@@ -1,6 +1,14 @@
 package pl.training.microservices.payments;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PaymentsRepository extends JpaRepository<Payment, Long> {
+
+    @Query("select p from Payment p where p.status = :status")
+    List<Payment> getByStatus(@Param("status") PaymentStatus status);
+
 }
